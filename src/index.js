081d448f79226aaa20a5a7f17b80925cc94ba37e
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-06-18 21:52:37 -0400

    Remove reporting web vitals

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { ColorModeScript } from "@chakra-ui/react";
 import './index.css';
 import App from './App';
 import theme from "./theme";
-import reportWebVitals from './reportWebVitals';
-// 1. Import the extendTheme function
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,8 +15,3 @@ root.render(
     </React.StrictMode>
   </ChakraProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
